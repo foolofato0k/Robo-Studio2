cd09ecfc3768634b24def70430f3333d44710f2e
William Foreman <william.l.foreman@student.uts.edu.au>
2025-03-30 00:05:17 +1100

    Will merging control progress original

---
+++ control/launch/mtc_test.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    package_share_dir = get_package_share_directory('test')
+    config_dir = os.path.join(package_share_dir, 'config')
+
+    moveit_config = MoveItConfigsBuilder(robot_name="ur3e", package_name="test")
+    moveit_config.robot_description_kinematics(os.path.join(config_dir, "kinematics.yaml"))
+    moveit_config.planning_pipelines(pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"])
+    moveit_config = moveit_config.to_moveit_configs()
+
+    move_group_capabilities = {
+        "capabilities": "move_group/ExecuteTaskSolutionCapability"
+    }
+
+    return LaunchDescription([
+        Node(
+            package="test",
+            executable="mtc_main",
+            output="screen",
+            parameters=[
+                moveit_config.to_dict(),
+                move_group_capabilities
+            ]
+        )
+    ])
---
+++ control/src/rs_moveit.cpp
@@ -0,0 +1,20 @@
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/planning_scene/planning_scene.hpp>
+#include <moveit/planning_scene_interface/planning_scene_interface.hpp>
+#include <moveit/task_constructor/task.h>
+#include <moveit/task_constructor/solvers.h>
+#include <moveit/task_constructor/stages.h>
+#if __has_include(<tf2_geometry_msgs/tf2_geometry_msgs.hpp>)
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#else
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#endif
+#if __has_include(<tf2_eigen/tf2_eigen.hpp>)
+#include <tf2_eigen/tf2_eigen.hpp>
+#else
+#include <tf2_eigen/tf2_eigen.h>
+#endif
+
+static const rclcpp::Logger LOGGER = rclcpp::get_logger("mtc_tutorial");
+namespace mtc = moveit::task_constructor;
+
