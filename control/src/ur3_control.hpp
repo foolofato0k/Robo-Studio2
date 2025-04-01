9b9763b74b1ebe19ee87e046117da673895fa1b3
William Foreman <william.l.foreman@student.uts.edu.au>
2025-04-02 10:10:39 +1100

    Upload for Arhma

---
+++ control/src/ur3_control.hpp
@@ -0,0 +1,42 @@
+#ifndef UR3_CONTROL_HPP_
+#define UR3_CONTROL_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit_msgs/msg/collision_object.hpp>
+#include <shape_msgs/msg/solid_primitive.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+#include <vector>
+#include <string>
+
+
+class UR3Control : public rclcpp::Node
+{
+public:
+    UR3Control();
+
+private:
+    // ROS2 Subscribers
+    rclcpp::Subscription<geometry_msgs::msg::PoseArray>::SharedPtr goals_subscriber_;
+
+    // Moveit Interfaces
+    moveit::planning_interface::MoveGroupInterface move_group_;
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface_;
+
+    // Internal state
+    std::vector<geometry_msgs::msg::Pose> waypoints_;
+    geometry_msgs::msg::PoseArray pose_array_;
+
+    // Member functions
+    void goalsCallback(const geometry_msgs::msg::PoseArray::SharedPtr msg);
+    void planAndExecute(const std::vector<geometry_msgs::msg::Pose>& waypoints);
+
+    std::vector<moveit_msgs::msg::CollisionObject> createCollisionObjects(
+        const std::vector<geometry_msgs::msg::Pose>& waypoints,
+        const std::string& planning_frame)
+    std::vector<geometry_msgs::msg::Pose> updateWaypointsForGrasping(
+        const std::vector<geometry_msgs::msg::Pose>& original_waypoints, double offset_z)
+}
+
+#endif  // UR3_CONTROL_HPP_
