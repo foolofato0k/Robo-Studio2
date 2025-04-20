5f557a6f139f2b3e82532d424d1a03ea859f7cb5
Jesse-G0nzalez <jesse.gonzalez@student.uts.edu.au>
2025-04-20 14:51:52 +1000

    i got the node to publish true to the terminal

---
+++ gui/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'gui'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='jesse',
+    maintainer_email='jesse.gonzalez@student.uts.edu.au',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'gui_node = gui.gui_node:main'
+        ],
+    },
+)
