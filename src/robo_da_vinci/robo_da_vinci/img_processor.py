0196b787bb5ad98c01e36d975dc7cb424f808c3f
Arhma <arhma.baig@student.uts.edu.au>
2025-04-23 13:54:03 +1000

    tried to sort out img processing a bit more

--- src/robo_da_vinci/robo_da_vinci/img_processor.py
+++ src/robo_da_vinci/robo_da_vinci/img_processor.py
@@ -23,82 +23,73 @@ def detectFaceEdges(img):
     roi = img.copy()
     for (x,y,w,h) in faces:
         roi = roi[y:y+h, x:x+w]
-    roi = cv.resize(roi, (200, 200)) # for canvas creation - can comment out if needed
+    
+    roi = cv.resize(roi, (250, 250)) # resized to a standard size for canvas creation 
     
 
     # EDGE DETECTION ______________________
     # Add Gussian Blur
-    img_blur = cv.GaussianBlur(roi, (3,3), 0)
+    img_blur = cv.GaussianBlur(roi, (5,5), 0)
     # Detect Edges
     edge_img = cv.Canny(image=img_blur, threshold1=20, threshold2=120) # Canny Edge Detection
 
     # Try to clean image
-    kernel = np.ones((5, 5), np.uint8)
+    kernel = np.ones((3, 3), np.uint8)
     edge_img = cv.morphologyEx(edge_img, cv.MORPH_CLOSE, kernel)
 
-    #poster = createPoster(edge_img)
-    #return poster
+    edge_img = createPoster(edge_img) # without text
+
     return edge_img
 
 def createPoster(img): # private function
         
-        # CREATE POSTER - based on face image shape ___________________________
-        buffer = 2
-        w, h = img.shape
-        canvas = np.zeros((w*buffer,h*buffer),dtype=np.uint8) # create poster
-        cw, ch = canvas.shape
-
-        # Casting the original image into the center of the canvas
-        o_w = int(cw/buffer) - int(w/buffer)
-        o_h = int(ch/buffer) - int(h/buffer)
-        
-        canvas[o_w:w+o_w, o_h:h+o_h] = img
-        canvas = cv.rectangle(canvas,(o_w,o_h),(w+o_w,h+o_h),color=255,thickness=1)
+        canvas = np.zeros((400,400),dtype=np.uint8) # create poster
+        canvas[75:325, 75:325] = img
+        cv.rectangle(canvas,(75,75),(325,325),color=255,thickness=1)
 
         # ADD TEXT_______________________________
-        font_large = 5 # Scale text relative to canvas size
-        font_small = font_large * 0.5  # Smaller text
+        #Heading = 'WANTED'
+        #subtext1 = 'DEAD OR ALIVE'
+        #subtext2 = '$10'
 
-        wanted_pos = (o_w-50, o_h-10)  # Top part
-        dead_or_alive_pos = (o_w-50, ch-50)  # Bottom part
-        reward_pos = (o_w+50, ch-10)  # Near the bottom center
-
-        canvas = cv.putText(canvas, 'WANTED', org=wanted_pos, fontFace=cv.FONT_HERSHEY_PLAIN, fontScale=font_large, color=255, thickness=1)
-        canvas = cv.putText(canvas, 'DEAD OR ALIVE', org=dead_or_alive_pos, fontFace=cv.FONT_HERSHEY_PLAIN, fontScale=font_small, color=255, thickness=1)
-        canvas = cv.putText(canvas, '$10', org=reward_pos, fontFace=cv.FONT_HERSHEY_PLAIN, fontScale=font_small, color=255, thickness=1)
+        #font_large = 5 # Scale text relative to canvas size
+        #font_small = font_large * 0.5  # Smaller text
+#
+        #heading_pos = (50, 90)  # Top part
+        #subtext1_pos = (50, 350)  # Bottom part
+        #subtext2_pos = (150, 390)  # Near the bottom center
+#
+        #canvas = cv.putText(canvas, Heading, org=heading_pos, fontFace=cv.FONT_HERSHEY_PLAIN, fontScale=font_large, color=255, thickness=1)
+        #canvas = cv.putText(canvas, subtext1, org=subtext1_pos, fontFace=cv.FONT_HERSHEY_PLAIN, fontScale=font_small, color=255, thickness=1)
+        #canvas = cv.putText(canvas, subtext2, org=subtext2_pos, fontFace=cv.FONT_HERSHEY_PLAIN, fontScale=font_small, color=255, thickness=1)
 
         return canvas
 
 def getPaths(img):
 
-    ### OLD VERSION of contour generation
-
-    # img = cv.threshold(img, 127, 255, cv.THRESH_BINARY)[1] # convert to black and white - with one chanel
-    # num_labels, labels_img = cv.connectedComponents(img) # get the groups - mess with the parameters later
-    # h, w = labels_img.shape
-
-    # paths = []
-    # pt_type = None
-
-    # for label in range(num_labels):
-
-    #     mask = (labels_img == label).astype(np.uint8) 
-    #     bitmap = potrace.Bitmap(mask.astype(bool))
-    #     path = bitmap.trace()
-
-    #     paths.append(path)
-
-    ### NEW VERSION of contour generation
-    contours, _ = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
+    ### OLD VERSION of contour generation - publishes 1586 strokes
+    img = cv.threshold(img, 127, 255, cv.THRESH_BINARY)[1] # convert to black and white - with one chanel
+    num_labels, labels_img = cv.connectedComponents(img) # get the groups
     paths = []
-    for cnt in contours:
-        mask = np.zeros_like(img)
-        cv.drawContours(mask, [cnt], -1, color=255, thickness=cv.FILLED)
+    for label in range(num_labels):
+        if label == 0: # first label is the background
+            continue
+        else:
+            mask = (labels_img == label).astype(np.uint8) 
+            bitmap = potrace.Bitmap(mask.astype(bool))
+            path = bitmap.trace()
+            paths.append(path)
 
-        bitmap = potrace.Bitmap(mask.astype(bool))
-        path = bitmap.trace()
-        paths.append(path)
+    ### NEW VERSION of contour generation - publishes 3185 strokes
+    #contours, _ = cv.findContours(img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    #paths = []
+    #for cnt in contours:
+    #    mask = np.zeros_like(img)
+    #    cv.drawContours(mask, [cnt], -1, color=255, thickness=cv.FILLED)
+    #    bitmap = potrace.Bitmap(mask.astype(bool))
+    #    path = bitmap.trace()
+    #    paths.append(path)
+    
     return paths
 
 def WebcamImg(image):
@@ -156,7 +147,9 @@ if __name__ == '__main__':
             exit()
             
     else:
-        image = cv.imread('test_images/webcam_img.jpg')
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        image_path = os.path.join(script_dir, 'test_images', 'webcam_img.jpg')
+        image = cv.imread(image_path)
     
     if image is None:
         print("Failed to load image. Check the path: test_images/webcam_img.jpg")
@@ -179,13 +172,9 @@ if __name__ == '__main__':
     fig, ax = plt.subplots(figsize=(6, 6))
     i = 0
     for path in paths:
-        if i == 0:
-            i += 1
-            continue
         for curve in path:
             curve_verts = curve.tesselate()
             x, y = zip(*curve_verts)
             ax.scatter(x, y, marker='o', s=2, label=f'Group {i}')
-
         i += 1
     plt.show()
